6378cb48ee9b469a01ae7a53174c5575cb7d3e6a
Deepan <deepan.seeralan@gmail.com>
2018-02-28 21:30:43 -0500

    added deletion function and test cases

--- bst/bst.py
+++ bst/bst.py
@@ -1,4 +1,4 @@
-class Node(object): # Is object really needed? What is the significance?
+class Node: # Is object really needed? What is the significance?
     def __init__(self, value, parent = None, leftChild = None, rightChild = None):
         self.value = value
         self.left = leftChild
@@ -6,16 +6,31 @@ class Node(object): # Is object really needed? What is the significance?
         self.parent = parent
     
     def has_left_child(self):
-        return self.left != None
+        return self.left is not None
     
     def has_right_child(self):
-        return self.right != None
+        return self.right is not None
+    
+    def has_parent(self):
+        return self.parent is not None
+    
+    def is_root(self):
+        return self.parent is None
     
     def has_no_children(self):
-        return (not self.has_left_child()) and (not self.has_right_child())
+        return (not (self.has_left_child() or self.has_right_child()))
+    
+    def has_one_child(self):
+        return (self.left and not self.right) or (self.right and not self.left)
     
     def has_both_children(self):
         return (self.has_left_child() and self.has_right_child())
+    
+    def is_left_child(self):
+        return self.has_parent() and self.parent.left == self
+    
+    def is_right_child(self):
+        return self.has_parent() and self.parent.right == self
     
     def get_height(self):
         if self.has_no_children():
@@ -41,7 +56,56 @@ class Node(object): # Is object really needed? What is the significance?
         while (self.has_right_child()):
             return self.right.find_max()
         return self
-class BinarySearchTree(object):
+    
+    def replace_data(self, new_val, new_left, new_right):
+        self.value = new_val
+        self.left = new_left
+        self.right = new_right
+        if self.has_left_child():
+            self.left.parent = self
+        if self.has_right_child():
+            self.right.parent = self
+    
+    def find_successor(self):
+        # Successor is the next highest node of the current node.
+        successor = None
+        if self.has_right_child():
+            successor = self.right.find_min()
+        else:
+            if self.has_parent():
+                if self.is_left_child():
+                    successor = self.parent
+                else:
+                    # node has no right child and is the right child of its parent
+                    # its successor is the sucessor of its parent without this node
+                    self.parent.right = None
+                    successor = self.parent.find_successor()
+                    self.parent.right = self
+        return successor
+    
+    def detach(self):
+        if self.has_no_children():
+            if self.is_left_child():
+                self.parent.left = None
+            else:
+                self.parent.right = None
+        elif self.has_one_child():
+            if self.has_left_child():
+                if self.is_left_child():
+                    self.parent.left = self.left
+                else:
+                    self.parent.right = self.left
+                self.left.parent = self.parent
+            else:
+                if self.is_left_child():
+                    self.parent.left = self.right
+                else:
+                    self.parent.right = self.right
+                self.right.parent = self.parent
+        else:
+            raise AssertionError("Not a leaf node")
+
+class BinarySearchTree:
     def __init__(self):
         self.root = None
         self.node_count = 0
@@ -71,6 +135,66 @@ class BinarySearchTree(object):
                 self.node_count += 1
         else:
             pass # Value exists already
+    
+    def find(self, value):
+        if self.root is None:
+            return None
+        return self._find(self.root, value)
+        
+    def _find(self, node, value):
+        if node is None:
+            return None
+        elif value == node.value:
+            return node
+        elif value < node.value:
+            return self._find(node.left, value)
+        else:
+            return self._find(node.right, value)
+
+    def delete(self, value):
+        node_to_delete = self.find(value)
+        if node_to_delete is None:
+            raise KeyError
+        if node_to_delete == self.root and node_to_delete.has_no_children():
+            self.root = None
+            self.node_count = 0
+        else:
+            self._delete(node_to_delete)
+            self.node_count -= 1
+    
+    def _delete(self, node):
+        if node.has_no_children():
+            if node.is_left_child():
+                node.parent.left = None
+            else:
+                node.parent.right = None
+            # root-only case is handled in delete() itself.
+        elif node.has_one_child():
+            if node.has_left_child():
+                if node.is_left_child():
+                    node.parent.left = node.left
+                    node.left.parent = node.parent
+                elif node.is_right_child():
+                    node.parent.right = node.left
+                    node.left.parent = node.parent
+                else:
+                    # Root node. Just replace the node contents
+                    node.replace_data(node.left.value, node.left.left, node.left.right)
+            else:
+                if node.is_left_child():
+                    node.parent.left = node.right
+                    node.right.parent = node.parent
+                elif node.is_right_child():
+                    node.parent.right = node.right
+                    node.right.parent = node.parent
+                else:
+                    # Root node. Just replace the node contents
+                    node.replace_data(node.right.value, node.right.left, node.right.right)
+        else:
+            # Node has both left and right child.
+            successor = node.find_successor()
+            successor.detach()
+            node.value = successor.value
     
     def get_values_in_order(self):
         values_in_order = []
--- bst/test_bst.py
+++ bst/test_bst.py
@@ -2,7 +2,6 @@ import unittest
 import bst
 
 class TestBinarySearchTree(unittest.TestCase):
-    
     def test_emptyTree(self):
         emptyTree = bst.BinarySearchTree()
         self.assertEqual(emptyTree.get_height(), 0)
@@ -34,6 +33,69 @@ class TestBinarySearchTree(unittest.TestCase):
         self.assertEqual(sorted(test_inputs), expectedResult, "Returned values not inorder")
         self.assertEqual(test_tree.get_height(), 4, "Height didn't match with the test tree")
         self.assertEqual(len(test_inputs), len(test_tree), "Invalid node count")
+    
+    def test_delete_empty_tree(self):
+        test_tree = bst.BinarySearchTree()
+        with self.assertRaises(KeyError):
+            test_tree.delete(10)
+    
+    def test_delete_missing_value(self):
+        test_tree = bst.BinarySearchTree()
+        test_tree.insert(100)
+        with self.assertRaises(KeyError):
+            test_tree.delete(10)
+
+    def test_delete_root_only_tree(self):
+        test_tree = bst.BinarySearchTree()
+        test_tree.insert(10)
+        test_tree.delete(10)
+        self.assertEqual(len(test_tree), 0, "Unexpected node found after deleting the root")
+    
+    def test_delete_leaf_node(self):
+        test_tree = bst.BinarySearchTree()
+        input_values = [15, 10, 25]
+        for i in input_values:
+            test_tree.insert(i)
+        test_tree.delete(10)
+        self.assertEqual(test_tree.find(10), None, "Deleted node found after deletion")
+    
+    def test_delete_single_child_node(self):
+        test_tree = bst.BinarySearchTree()
+        input_values = [5, 10, 25]
+        for i in input_values:
+            test_tree.insert(i)
+        test_tree.delete(10)
+        self.assertEqual(test_tree.find(10), None, "Deleted node found after deletion")
+    
+    def test_delete_two_child_node(self):
+        test_tree = bst.BinarySearchTree()
+        input_values = [15, 10, 25, 20, 30]
+        value_to_delete = 25
+        for i in input_values:
+            test_tree.insert(i)
+        test_tree.delete(value_to_delete)
+        self.assertEqual(test_tree.find(value_to_delete),
+                         None,
+                         "Deleted node found after deletion")
+        input_values.remove(value_to_delete)
+        self.assertEqual(sorted(input_values),
+                         test_tree.get_values_in_order(), 
+                         "Deleted node found after deletion")
+    
+    def test_delete_root_node(self):
+        test_tree = bst.BinarySearchTree()
+        input_values = [15, 10, 25, 20, 30]
+        value_to_delete = 15
+        for i in input_values:
+            test_tree.insert(i)
+        test_tree.delete(value_to_delete)
+        self.assertEqual(test_tree.find(value_to_delete),
+                         None,
+                         "Deleted node found after deletion")
+        input_values.remove(value_to_delete)
+        self.assertEqual(sorted(input_values),
+                         test_tree.get_values_in_order(), 
+                         "Deleted node found after deletion")
 
 
 if __name__ == '__main__':
