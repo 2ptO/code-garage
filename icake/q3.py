738aaac43a90a791162d4fe52c1bf45c221594ea
Deepan <deepan.seeralan@gmail.com>
2018-03-06 07:39:11 -0500

    removed non-ascii chars in q3

--- icake/q3.py
+++ icake/q3.py
@@ -1,8 +1,8 @@
 # Your company built an in-house calendar tool called HiCal. You want to add
 # a feature to see the times in a day when everyone is available.
 
-# To do this, you’ll need to know when any team is having a meeting. In HiCal,
-# a meeting is stored as a tuple ↴ of integers (start_time, end_time).
+# To do this, you will need to know when any team is having a meeting. In HiCal,
+# a meeting is stored as a tuple of integers (start_time, end_time).
 # These integers represent the number of 30-minute blocks past 9:00am.
 
 # Write a function merge_ranges() that takes a list of multiple meeting time ranges and returns a list of condensed ranges.
