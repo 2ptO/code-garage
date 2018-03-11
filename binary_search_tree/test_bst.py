89d824efdc863630c01889916c3d8452fd6122f4
Deepan <deepan.seeralan@gmail.com>
2018-03-11 19:28:59 -0400

    resolved version issues with statistics module and yield from keyword

--- binary_search_tree/test_bst.py
+++ binary_search_tree/test_bst.py
@@ -1,4 +1,5 @@
 import unittest
+from sys import version_info as py_version
 import bst
 
 class TestBinarySearchTree(unittest.TestCase):
@@ -86,6 +87,7 @@ class TestBinarySearchTree(unittest.TestCase):
                          self.test_tree.get_values_in_order(), 
                          "Deleted node found after deletion")
     
+    @unittest.skipIf(py_version < (3, 3), "yield from supported only from 3.3")
     def test_inorder_generator(self):
         input_values = []
         
