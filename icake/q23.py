89a2b0ab7b83ebfa6f34f062d03d566f5ee5bc88
Deepan <deepan.seeralan@gmail.com>
2018-03-20 20:56:31 -0400

    minor comments to q23

--- icake/q23.py
+++ icake/q23.py
@@ -1,6 +1,6 @@
 # Write a function contains_cycle() that takes the first node in a singly-linked list
 # and returns a boolean indicating whether the list contains a cycle.
-#
+# Skipped Q22 which was a simple question on link list.
 #
 class LinkedListNode:
     """
@@ -15,6 +15,7 @@ def contains_cycle(head):
     Return true if the list pointed by head contains a cycle
     """
     if head is None:
+        # Consider no cycle if there is no head node
         return False
     
     first = head
